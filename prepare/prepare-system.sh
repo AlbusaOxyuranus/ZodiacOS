1374e02758a75accd2f6eabedea5eecf40455b99
AlbusaOxyuranus <dionis_999@hotmail.com>
2017-03-28 19:10:54 +0300

    add prepare system

---
+++ prepare/prepare-system.sh
@@ -0,0 +1,12 @@
+#prepare system, download and install tools
+clear
+sudo apt-get install build-essential;
+sudo apt-get install bin86 kvm qemu gcc build-essential
+sudo apt-get install g++;
+sudo apt-get install gcc;
+sudo apt-get install dd;
+sudo apt-get install ld;
+sudo apt-get install qemu-system-i386;
+sudo apt-get install gcc-multilib;
+sudo apt-get install g++-multilib;
+sudo apt-get update;
