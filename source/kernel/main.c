1a719899a2b74c6700e3ad95c7c6391b330d2b3e
Denis Prokhorchik <dionis_999@hotmail.com>
2017-03-29 22:14:30 +0300

    fix

--- source/kernel/main.c
+++ source/kernel/main.c
@@ -12,11 +12,21 @@ void kernel_main(uint8 boot_disk_id, void *memory_map, BootModuleInfo *boot_modu
 	init_memory_manager(memory_map);
 	init_interrupts();
 	init_tty();
-	set_text_attr(15);
+	set_text_attr(10);
 	printf("Welcome to Zodiac OS!\n");
 	
 	printf("kernel_page_dir = 0x%x\n", kernel_page_dir);
 	printf("memory_size = %d MB\n", memory_size / 1024 / 1024);
         printf("get_page_info(kernel_page_dir, 0xB8000) = 0x%x\n", get_page_info(kernel_page_dir, (void*)0xB8000));
 
+	while (true) {
+		char buffer[256];
+		
+		out_string("ZodiacOS$: ");
+		in_string(buffer, sizeof(buffer));
+		
+		printf("You typed: %s\n", buffer);
+		
+	}
 } 
+
