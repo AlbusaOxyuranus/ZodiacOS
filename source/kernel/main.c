3f3763f1751973de35ebe287b48384ffa9f4582a
Denis Prokhorchik <dionis_999@hotmail.com>
2017-03-30 18:32:51 +0300

    edits

--- source/kernel/main.c
+++ source/kernel/main.c
@@ -1,32 +1,17 @@
-#include "stdlib.h"
-#include "memory_manager.h"
-#include "interrupts.h"
-#include "tty.h"
+#include<string.h>
 
 typedef struct {
-	uint64 base;
-	uint64 size;
+	unsigned long long base;
+	unsigned long long size;
 } BootModuleInfo;
 
-void kernel_main(uint8 boot_disk_id, void *memory_map, BootModuleInfo *boot_module_list) {
-	init_memory_manager(memory_map);
-	init_interrupts();
-	init_tty();
-	set_text_attr(10);
-	printf("Welcome to Zodiac OS!\n");
-	
-	printf("kernel_page_dir = 0x%x\n", kernel_page_dir);
-	printf("memory_size = %d MB\n", memory_size / 1024 / 1024);
-        printf("get_page_info(kernel_page_dir, 0xB8000) = 0x%x\n", get_page_info(kernel_page_dir, (void*)0xB8000));
-
-	while (true) {
-		char buffer[256];
-		
-		out_string("ZodiacOS$: ");
-		in_string(buffer, sizeof(buffer));
-		
-		printf("You typed: %s\n", buffer);
-		
+void kernel_main(char boot_disk_id, void *memory_map, BootModuleInfo *boot_module_list) {
+	char *screen_buffer = (void*)0xB8000;
+	char *msg = "Zodiac OS - HELLO!";
+	unsigned int i = 24 * 80;
+	while (*msg) {
+		screen_buffer[i * 2] = *msg;
+		msg++;
+		i++;
 	}
 } 
-
